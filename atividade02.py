17ff98fafb5b847dbe6fd8007addda47bafc709a
João Victor <70414422+bonfimgit@users.noreply.github.com>
2024-09-10 23:25:57 -0300

    Add files via upload

---
+++ atividade02.py
@@ -0,0 +1,5 @@
+dia = input ('Dia do nascimento: ')
+mes = input ('Mes do nascimento: ')
+ano = input ('Ano do nascimento: ')
+
+print ('Você nasceu no dia', dia, 'de', mes, 'de', ano,' . Correto?')
---
+++ atividade03.py
@@ -0,0 +1,4 @@
+num1 = input ('Primeiro numero: ')
+num2 = input ('Segundo numero: ')
+
+print ('a soma é: ', num1 + num2)
